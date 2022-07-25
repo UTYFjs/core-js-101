2c2bf2f48df0bcc3bbf457e96b5f7809a4012a12
utyfjs <belarus1990@gmail.com>
2022-07-26 00:52:15 +0400

    fix: fix linter issue

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -165,7 +165,7 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(fn, ...args1) {
-  return function (...args) { return fn(...args1, ...args); };
+  return function a(...args) { return fn(...args1, ...args); };
 }
 
 
