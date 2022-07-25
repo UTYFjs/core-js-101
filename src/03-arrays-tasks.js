f0ddaef70ab0c9563dcb88be765e3c7c59a3fd10
utyfjs <belarus1990@gmail.com>
2022-07-26 02:40:14 +0400

    feat: done 84 tests

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -603,8 +603,12 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const middlePoint = Math.floor(arr.length / 2);
+  const head = arr.slice(0, middlePoint);
+  const tail = arr.slice(middlePoint);
+  if (arr.length % 2 === 0) return tail.concat(head);
+  return tail.slice(1).concat(arr[middlePoint]).concat(head);
 }
 
 
--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -53,8 +53,11 @@ function parseDataFromIso8601(value) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (year % 4 !== 0) { return false; }
+  if (year % 100 !== 0 || year % 400 === 0) { return true; }
+  return false;
 }
 
 
@@ -94,8 +97,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hour = date.getUTCHours() > 12 ? date.getUTCHours() - 12 : date.getUTCHours();
+  const minute = date.getUTCMinutes();
+  let angle = Math.abs(0.5 * (60 * hour - 11 * minute));
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+  return Math.abs(((angle * Math.PI) / 180));
 }
 
 
